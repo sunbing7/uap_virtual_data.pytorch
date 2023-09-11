9d117eb7fb7573e003a0b40c706867abc552c76f
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-09-11 21:47:12 +0800

    test

--- run_act.sh
+++ run_act.sh
@@ -8,8 +8,8 @@ for tgt in ${TARGET_CLASSES[@]}; do
     python analyze_input.py --option=analyze_clean --causal_type=act --targeted=True --dataset=imagenet --arch=vgg19 --model_name=vgg19_imagenet.pth --seed=123 --num_iterations=50 --result_subfolder=result --target_class=$tgt --split_layer=43 --batch_size=32 --ngpu=1 --workers=4
     python analyze_input.py --option=analyze_layers --analyze_clean=1 --causal_type=act --targeted=True --dataset=imagenet --arch=vgg19 --model_name=vgg19_imagenet.pth --seed=123 --num_iterations=50 --result_subfolder=result --target_class=$tgt --split_layer=43 --batch_size=32 --ngpu=1 --workers=4
 
-    python analyze_input.py --option=calc_pcc --causal_type=act --idx=$IDX --target_class=$tgt --num_iterations=0 --split_layer=43
-    python analyze_input.py --option=calc_pcc --causal_type=act --analyze_clean=1 --num_iterations=50 --target_class=$tgt --split_layer=43
+    python analyze_input.py --option=calc_entropy --causal_type=act --idx=$IDX --target_class=$tgt --num_iterations=0 --split_layer=43
+    python analyze_input.py --option=calc_entropy --causal_type=act --analyze_clean=1 --num_iterations=50 --target_class=$tgt --split_layer=43
     ((IDX++))
     #echo $IDX
     #echo $tgt
