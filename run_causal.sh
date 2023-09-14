90fdf29b3f436ba886e8ee3a43691b977fb1137b
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-09-14 12:47:15 +0800

    test

--- run_causal.sh
+++ run_causal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for TARGET_CLASS in {39,527,65,693,771}
+for TARGET_CLASS in {150,214,39,527,65,693,771}
 do
   echo "Analyzing target class:" $TARGET_CLASS
   #python train_uap.py --dataset=imagenet --pretrained_dataset=imagenet --pretrained_arch=vgg19 --model_name=vgg19_cifar10.pth --pretrained_seed=123 --epsilon=0.0392 --num_iterations=1000 --result_subfolder=result --loss_function=bounded_logit_fixed_ref --confidence=10 --targeted=True --target_class=$TARGET_CLASS --ngpu=1 --workers=4 --batch_size=32 --learning_rate=0.005
