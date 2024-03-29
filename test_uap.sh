256ea452cd2565920b29bb00eed088492a09ffa9
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2024-03-29 20:06:18 +0800

    split layer for resnet

--- test_uap.sh
+++ test_uap.sh
@@ -4,7 +4,7 @@
 #do
 #    python test_uap.py --targeted=True --dataset=imagenet --pretrained_dataset=imagenet --test_name=vgg19_imagenet.pth --pretrained_arch=vgg19 --pretrained_seed=123 --test_dataset=imagenet --test_arch=vgg19 --result_subfolder=result --targeted=True --target_class=$TARGET_CLASS --ngpu=1 --workers=4
 #done
-
+################################################################################################################################################
 #resnet50
 for TARGET_CLASS in {755,743,804,700,922,174,547,369}
 do
