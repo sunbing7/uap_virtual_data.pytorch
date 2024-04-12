45cf196962861db8c8c5c7effafd97fcf2d52845
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2024-04-12 17:03:38 +0800

    google net

--- train_uap.sh
+++ train_uap.sh
@@ -37,7 +37,7 @@
 ################################################################################################################################################
 #googlenet_caffe
 
-#for TARGET_CLASS in {573,807,541,240,475,753,762,505}
+for TARGET_CLASS in {573,807,541,240,475,753,762,505}
 do
     python train_uap_caffe.py --dataset=imagenet --pretrained_dataset=imagenet --pretrained_arch=googlenet --pretrained_seed=123 --epsilon=0.0392 --num_iterations=1000 --result_subfolder=result --loss_function=bounded_logit_fixed_ref --confidence=10 --targeted=True --target_class=$TARGET_CLASS --ngpu=1 --workers=4 --batch_size=32 --learning_rate=0.005
 done
